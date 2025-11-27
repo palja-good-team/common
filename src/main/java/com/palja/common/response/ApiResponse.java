b9937d7f359307ae44feeea0deb5b8160fe3a585
geon <leegeonhee0204@naver.com>
2025-11-27 11:06:45 +0900

    fix: ApiResponse code 필드 변경

--- src/main/java/com/palja/common/response/ApiResponse.java
+++ src/main/java/com/palja/common/response/ApiResponse.java
@@ -6,29 +6,29 @@ import com.palja.common.exception.CommonErrorCode;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T>(
 	boolean success,
+	String code,
 	String message,
-	T data,
-	String code
+	T data
 ) {
-
-	/** 성공 응답 생성 (데이터 + 메시지) */
-	public static <T> ApiResponse<T> success(T data, String message) {
-		return new ApiResponse<>(true, message, data, null);
-	}
 
 	/** 성공 응답 생성 (메시지) */
 	public static <T> ApiResponse<T> success(String message) {
-		return new ApiResponse<>(true, message, null, null);
+		return new ApiResponse<>(true, "OK", message, null);
+	}
+
+	/** 성공 응답 생성 (데이터 + 메시지) */
+	public static <T> ApiResponse<T> success(T data, String message) {
+		return new ApiResponse<>(true, "OK", message, data);
 	}
 
 	/** 실패 응답 생성 (에러 코드 + 에러 메시지) */
 	public static <T> ApiResponse<T> error(CommonErrorCode errorCode) {
-		return new ApiResponse<>(false, errorCode.getMessage(), null, errorCode.getCode());
+		return new ApiResponse<>(false, errorCode.getHttpStatus().name(), errorCode.getMessage(), null);
 	}
 
 	/** 실패 응답 생성 (에러 메시지 + 메시지) */
 	public static <T> ApiResponse<T> error(CommonErrorCode errorCode, String message) {
-		return new ApiResponse<>(false, errorCode.getMessage() + " (" + message + ")", null, errorCode.getCode());
+		return new ApiResponse<>(false, errorCode.getHttpStatus().name(), errorCode.getMessage() + " (" + message + ")", null);
 	}
 
 }
