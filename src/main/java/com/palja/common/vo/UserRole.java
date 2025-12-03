625e05336156652624def7ea51a15d3e9861080d
geon <leegeonhee0204@naver.com>
2025-12-03 16:41:05 +0900

    fix: UserRole에 COMPANY_USER 추가

--- src/main/java/com/palja/common/vo/UserRole.java
+++ src/main/java/com/palja/common/vo/UserRole.java
@@ -1,9 +1,15 @@
 package com.palja.common.vo;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
 
 	MASTER,
 	MANAGER,
 	CUSTOMER,
+	COMPANY_USER
 
 }
